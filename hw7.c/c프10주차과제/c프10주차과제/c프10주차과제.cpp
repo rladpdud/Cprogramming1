8cbe605e008ab699f00ad7199d0ca872a87ee39e
rladpdud <kimyy8821@naver.com>
2022-11-16 01:43:05 +0900

    Add File

---
+++ hw7.c/c프10주차과제/c프10주차과제/c프10주차과제.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void)
+{
+	int arr1[6] = { 1,2,3,4,5,6 };
+	int* ptr1 = &arr1[0];
+	int arr2[6] = { 7,8,9,10,11,12 };
+	int* ptr2 = &arr2[0];
+	int i, temp;
+	for (i = 0; i < 6; i++)
+	{
+		temp = *ptr1;
+		*ptr1 = *ptr2;
+		*ptr2 = temp;
+		ptr1++;
+		ptr2++;
+	}
+
+	for (i = 0; i < 6; i++)
+		printf("%d", arr1[i]);
+	for (i = 0; i < 6; i++)
+		printf("%d", arr2[i]);
+	return 0;
+}
